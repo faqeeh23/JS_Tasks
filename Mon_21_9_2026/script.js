2a65762f4eeb81868bfdd530e9940b8ba10021a8
Mohammed Al-faqeeh <mohammadaf200477@gmail.com>
2026-09-21 15:03:42 +0300

    Add initial HTML, JavaScript files, and README

---
+++ Mon_21_9_2026/script.js
@@ -0,0 +1,103 @@
+'use strict'
+
+/**Find the output for these expressions and justify the output according to JavaScript interpretation:
+● typeof(100)
+● typeof(73.9)
+● typeof(NaN)
+● typeof("Water")
+● typeof(false)
+● typeof(9 != 11)
+● "Orang" + "e"
+● "Orange" - "s"
+● "4" + "8"
+● "4" - "8"
+● "name" + 3
+● "name" - 3
+● 82 * "word"
+● 1 + "hello"
+● "hello" + 1
+● 1 + true
+● "hello" + true
+● typeof (Infinity)
+● 1 == '1'
+● 1 === '1' */
+
+
+
+console.log(typeof 100)
+
+// with give number 
+
+console.log(typeof(73.9))
+
+// will give number 
+
+console.log(NaN)
+
+// will give NaN 
+
+console.log(typeof("Water"))
+
+// will give string
+
+console.log(typeof(false))
+
+// will give boolen
+
+console.log(typeof(9 != 11));
+
+// will give boolen
+
+console.log("Orang" + "e");
+
+// Output Orange
+
+console.log("Orange" - "s");
+
+// Output NaN
+
+console.log("4" + "8");
+
+// Output 48
+
+console.log("name" + 3)
+
+// Output name3
+
+console.log("name" - 3);
+
+// Output NaN
+
+console.log(82 * "word");
+
+// Output NaN
+console.log(1 + "hello");
+// Output 1hello
+console.log("hello" + 1);
+// Output hello1
+console.log(1 + true);
+// Output 2
+console.log("hello" + true);
+// Output hellotrue
+console.log(typeof (Infinity));
+// Output Number
+console.log(1 == '1');
+
+// Output true
+
+console.log( 1 === '1');
+
+// Output false 
+
+
+
+
+
+
+
+
+
+
+
+
+
